0c355bb014f2725bc3d8d07cfa254bf36343c002
Ved Mathai <vedu29@gmail.com>
2022-04-28 12:11:18 +0530

    Adding MD reader sidebar and navbar

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/:id" element={<MainPage />} />
     </Routes>
   </BrowserRouter>,
   root
--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,20 +1,32 @@
 import './main-page.css';
 
+
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import Navbar from '../../components/navbar/navbar';
 import Sidebar from '../../components/sidebar/sidebar';
+import ReactMarkdown from 'react-markdown'
+import sample from './sample.md'
 
 
 function MainPage() {
+  var [mdData, setMdData] = useState('');
+  const { id } = useParams();
+  fetch(sample)
+  .then(r => r.text())
+  .then(text => {
+    setMdData(text);
+  });
   return (
     <div className="App">
         <Sidebar/>
         <Navbar/>
-
-        <header className="App-header">
-            <p>
-            Welcome to Stacking up by Ved Mathai
-            </p>
-        </header>
+        <div className="content-container">
+          <div className="content">
+              <ReactMarkdown>{mdData}</ReactMarkdown>
+          </div>
+        </div>
     </div>
   );
 }
